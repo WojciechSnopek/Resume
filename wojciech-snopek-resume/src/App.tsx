aab61026299a5c7b868191f4d12cd9c8acc430e2
wojciechsnopek <wojciechsnopek2@gmail.com>
2023-01-23 21:17:12 +0100

    simplify styles, add basic navigation

--- wojciech-snopek-resume/src/App.tsx
+++ wojciech-snopek-resume/src/App.tsx
@@ -3,16 +3,23 @@ import resume_profile_picture from "./static/resume_profile_picture.png";
 
 function App() {
   return (
-    //     <div className="container">
-    //   <nav className="navbar">
-    //       <ul>
-    //           <li><a href="#home">Home</a></li>
-    //           <li><a href="#about">About</a></li>
-    //           <li><a href="#service">Service</a></li>
-    //           <li><a href="#contact">Contact</a></li>
-    //         </ul>
-    //       </nav>
-    <div className="holster">
+    <div className="main-container">
+      <nav className="navbar">
+        <ul>
+          <li>
+            <a href="#home">Home</a>
+          </li>
+          <li>
+            <a href="#about">About</a>
+          </li>
+          <li>
+            <a href="#service">Service</a>
+          </li>
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
       <div className="container y mandatory-scroll-snapping">
         <section id="home">
           <h1>FRONTEND MASTER SITE</h1>
@@ -20,8 +27,7 @@ function App() {
           <img
             src={resume_profile_picture}
             alt="resume_picture"
-            width={"30%"}
-            height={"auto"}
+            className="image"
           />
         </section>
         <section id="about">
