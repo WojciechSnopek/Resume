db40c21768cc50367a12d79a7552967a33d346b6
wojciechsnopek <wojciechsnopek2@gmail.com>
2023-01-23 20:56:39 +0100

    Add smooth scrolling, add photo, cleanup initial code

--- wojciech-snopek-resume/src/App.tsx
+++ wojciech-snopek-resume/src/App.tsx
@@ -1,24 +1,51 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import resume_profile_picture from "./static/resume_profile_picture.png";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          THIS SHOULD BE DEPLOYED
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    //     <div className="container">
+    //   <nav className="navbar">
+    //       <ul>
+    //           <li><a href="#home">Home</a></li>
+    //           <li><a href="#about">About</a></li>
+    //           <li><a href="#service">Service</a></li>
+    //           <li><a href="#contact">Contact</a></li>
+    //         </ul>
+    //       </nav>
+    <div className="holster">
+      <div className="container y mandatory-scroll-snapping">
+        <section id="home">
+          <h1>FRONTEND MASTER SITE</h1>
+          <p>Hello, I'm Wojtek!</p>
+          <img
+            src={resume_profile_picture}
+            alt="resume_picture"
+            width={"30%"}
+            height={"auto"}
+          />
+        </section>
+        <section id="about">
+          <h1>About</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi,
+            quis!
+          </p>
+        </section>
+        <section id="service">
+          <h1>Service</h1>
+          <p className="lead">
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi,
+            quis!
+          </p>
+        </section>
+        <section id="contact">
+          <h1>Contact</h1>
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Commodi,
+            quis!
+          </p>
+        </section>
+      </div>
     </div>
   );
 }
